506f2569ed4a6f0122ef66655e3d02485a7f3cac
Israel Oliveira <raloliver.com@gmail.com>
2020-04-12 13:27:52 +0100

    Fixed error of syntax on ejs

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(port, () => console.log(`listening server on port ${port}`))
