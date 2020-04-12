989dc6afbb118632dbc4ee441423b89a44694f71
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-12 23:20:46 +0100

    Add bcrypt to generate salte and crypt pass

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -10,6 +11,24 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+/**
+ * Before save (we need the scope context) generate salt and crypt pass
+ */
+UserSchema.pre('save', function (next) {
+    const user = this
+
+    if (!user.isModified('password')) {
+        return next()
+    }
+
+    bcrypt.genSalt(null, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash
+            next()
+        })
+    })
 })
 
 const User = mongoose.model('User', UserSchema)
