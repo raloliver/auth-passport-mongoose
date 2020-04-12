2bdd2d302c8b569b7a98fc4c27fc5012431d3f8e
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-13 00:03:06 +0100

    Create a method to check when user zero exists

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/auth-passport-mongoose'
+const User = require('./models/user')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+
+/**
+ * countDocuments to count how many documents it is on db
+ * check if has at least one user
+ */
+const createUserZero = async () => {
+  const total = await User.countDocuments({ username: 'raloliver' })
+  if (total === 0) {
+    const user = new User({
+      username: 'raloliver',
+      password: '123456!Qw'
+    })
+    await user.save()
+    console.log('User zero created!')
+  } else {
+    console.log('User zero already exists')
+  }
+}
+
 /**
  * #TODO: whats is render method?
  */
@@ -22,15 +42,11 @@ app.get('/', (req, res) => res.render('index'))
 mongoose
   .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
+    createUserZero()
     app.listen(port, () => console.log(`listening server on port ${port}`))
   })
   .catch(err => console.log(err))
 
-// const User = require('./models/user')
-// const user = new User({
-//   username: 'raloliver',
-//   password: '123456!Qw'
-// })
 
-// user.save(() => console.log('User save!'))
+
 
