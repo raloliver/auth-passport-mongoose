87e03acac18a36dc2c68658da8e886f640d892bd
Israel Oliveira <raloliver.com@gmail.com>
2020-04-12 14:05:09 +0100

    Configure mongoose properties to new version

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+const mongo = process.env.MONGODB || 'mongodb://localhost/auth-passport-mongoose'
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
-
+/**
+ * #TODO: whats is render method?
+ */
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(port, () => console.log(`listening server on port ${port}`))
+/**
+ * #TODO: how to use promise with mongoose?
+ */
+mongoose
+  .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => console.log(`listening server on port ${port}`))
+  })
+  .catch(err => console.log(err))
