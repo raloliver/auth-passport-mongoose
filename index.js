f964d4e84cbc3de10b198ca1b9a6a8e09e2a6d27
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-13 09:34:04 +0100

    Check password

--- index.js
+++ index.js
@@ -39,10 +39,12 @@ app.get('/login', (req, res) => res.render('login'))
 app.post('/login', async (req, res) => {
   const user = await User.findOne({ username: req.body.username })
   const isValid = await user.checkPassword(req.body.password)
-  res.send({
-    user,
-    isValid
-  })
+  if (isValid) {
+    req.session.user = user
+    res.redirect('/private/posts')
+  } else {
+    res.redirect('/login')
+  }
 })
 
 /**
