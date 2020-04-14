98ada2c700d51c13c771cf416dfde5a39690fe40
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-14 09:37:43 +0100

    Init list of posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const Post = ('../models/post')
 
-router.get('/', (req, res) => {
-    res.send('public posts')
+const Post = require('../models/post')
+
+router.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('posts/index', { posts })
 })
 
 module.exports = router
