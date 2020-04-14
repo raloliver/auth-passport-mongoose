99de49419b56d1eb4ccf48750dee7e29a70d3cb9
ilimaoliveira <israel.limaoliveira@altran.com>
2020-04-14 10:11:48 +0100

    Separeted private and public posts

--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,10 @@ const router = express.Router()
 const Post = require('../models/post')
 
 router.get('/', async (req, res) => {
-    let access = {}
-    if (!('user' in req.session)) {
-        access = { status: 'public' }
-    }
+    let access = { status: 'public' }
+    // if (!('user' in req.session)) {
+    //     access = { status: 'public' }
+    // }
     const posts = await Post.find(access)
     res.render('posts/index', { posts })
 })
